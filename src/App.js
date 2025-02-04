a28535c93d189f6d31a599bc6d0289f97961c1c9
Gabriela Muniz <gabrielasdsmuniz@gmail.com>
2025-02-03 22:48:41 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Banner from "./componentes/banner";
+import Formulario from "./componentes/formulario";
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
